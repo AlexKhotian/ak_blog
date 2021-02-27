7e723e5077dbc9039674e445c682d4cd8727b289
AlexKhotian <doctaww@gmail.com>
2021-02-27 16:20:11 +0100

    Add About

--- pages/about.js
+++ pages/about.js
@@ -10,9 +10,10 @@ export default function About() {
         </Head>
         <article>
           <h2 className={utilStyles.headingMd}>About</h2>
-          <p>foo</p>
-          <p>bar</p>
-          <p>bar2</p>
+          <p>I’m a Berlin-based software engineer with a background in the embedded systems and cloud solutions. Over the years, I have focused on the serverless concept.</p>
+          <p>I'm co-hosting the BlendIt podcast as well where we are talking about IT industry and area around it.</p>
+          <p>When I'm not writing a code or stuck in the meetings I enjoy me coffee routine and cooking some high kitchen meals (at least I try).</p>
+          <p>Feel free to reach out on LinkedIn in case of any question or just chatting.</p>
         </article>
       </Layout>
     )
