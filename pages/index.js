23428db00b542b16d4a74467fe7db1b3977b7200
AlexKhotian <doctaww@gmail.com>
2021-02-27 14:36:01 +0100

    Add favicon and fix dates

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home({ allPostsData }) {
               </Link>
               <br />
               <small className={utilStyles.lightText}>
-                <Date dateString={date} />
+                {date}
               </small>
             </li>
           ))}
